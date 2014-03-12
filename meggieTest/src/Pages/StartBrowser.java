9ee0145d89279a711f4e4a1d95c38f3e5482a9d7
fangbing <735699747@qq.com>
2014-03-12 17:14:01 +0800

    Add simpleLocate and form

--- meggieTest/src/Pages/StartBrowser.java
+++ meggieTest/src/Pages/StartBrowser.java
@@ -1,17 +1,9 @@
 package Pages;
 
-import java.io.File;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 public class StartBrowser {
-    public static void main(String[] args){
-    	File pathToBinary = new File("D:\\Program Files\\firefox.exe");
-		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
-		FirefoxProfile firefoxProfile = new FirefoxProfile();
-		FirefoxDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+    public void startBrowser(){
+    	WebDriver dr = new ChromeDriver();
     }
 }
