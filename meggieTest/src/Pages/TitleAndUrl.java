7a5ae5ab81296e375e8a6c59eda57217bedf1e58
fangbing <735699747@qq.com>
2014-03-12 12:35:09 +0800

    commit TitleAndUrl class

--- meggieTest/src/Pages/TitleAndUrl.java
+++ meggieTest/src/Pages/TitleAndUrl.java
@@ -1,12 +1,25 @@
 package Pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TitleAndUrl {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		 WebDriver dr = new ChromeDriver();
+		 Thread.sleep(2000);
+		 
+		 String url ="http://www.baidu.com";
+		 System.out.printf("now access %s \n",url);
+		 
+		 dr.get(url);
+		 Thread.sleep(2000);
+		 
+		 System.out.printf("title of current page is %s\n",dr.getTitle());
+		 System.out.printf("url of current page is %s\n",dr.getCurrentUrl());
+		 
+		 System.out.println("browser will be close");
+		 dr.quit();
+		 
 
 	}
 
