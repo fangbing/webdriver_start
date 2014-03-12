a1278e9b6f96a0f9b8a48a3aa7cb50652f253de4
fangbing <735699747@qq.com>
2014-03-12 12:22:55 +0800

    Add new

--- meggieTest/src/Pages/StartBrowser.java
+++ meggieTest/src/Pages/StartBrowser.java
@@ -1,9 +1,17 @@
 package Pages;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 public class StartBrowser {
     public static void main(String[] args){
-    	WebDriver dr = new ChromeDriver();
+    	File pathToBinary = new File("D:\\Program Files\\firefox.exe");
+		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+		FirefoxProfile firefoxProfile = new FirefoxProfile();
+		FirefoxDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
     }
 }
---
+++ meggieTest/src/Pages/TitleAndUrl.java
@@ -0,0 +1,13 @@
+package Pages;
+
+public class TitleAndUrl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
